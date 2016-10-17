20c22138e7e4a140f84bd27c6e20af47575576b7
Mylialolz <antoineperraudat@hotmail.fr>
2016-10-17 15:43:03 +0200

    push lol

--- app/src/main/java/com/example/antoine/projectandroid3a/LaunchActivity.java
+++ app/src/main/java/com/example/antoine/projectandroid3a/LaunchActivity.java
@@ -2,12 +2,18 @@ package com.example.antoine.projectandroid3a;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -16,12 +22,27 @@ public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_launch);
 
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        if(image != null){
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+            Glide.with(this).load(R.raw.animated_bike).into(imageViewTarget);
+        }
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //setContentView(R.layout.activity_launch);
+
         /*Création d'un bouton de recherche et implémentation d'un listener*/
-        final Button button = (Button) findViewById(R.id.buttonGo);
+        Button button = (Button) findViewById(R.id.buttonGo);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText inputZip = (EditText) findViewById(R.id.zipInput);
