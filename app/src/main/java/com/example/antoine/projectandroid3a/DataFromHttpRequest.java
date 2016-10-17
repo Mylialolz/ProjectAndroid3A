5a2c62a7b346435cd6a405aff21b5a2b06f44b0e
WerlingJ <werlingjoris@hotmail.fr>
2016-10-17 13:38:00 +0200

    Update DataFromHttpRequest.java

--- app/src/main/java/com/example/antoine/projectandroid3a/DataFromHttpRequest.java
+++ app/src/main/java/com/example/antoine/projectandroid3a/DataFromHttpRequest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 /**
  * Created by Antoine on 24/09/2016.
+   Interface tunnel entre un fragment et l'activité pour lancer une requête API
  */
 
 public interface DataFromHttpRequest {
